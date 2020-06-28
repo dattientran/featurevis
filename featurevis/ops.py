8a76ba4703503e0e3076e0a7faffcb2a14e476ac
Zhiwei Ding <zhiweid@bcm.edu>
2020-06-28 18:49:23 -0500

    fix bug in changemaskstats

--- featurevis/ops.py
+++ featurevis/ops.py
@@ -555,5 +555,5 @@ class ChangeMaskStats():
     def __call__(self, x):
         mask_mean = torch.sum(x * self.mask, (-1, -2), keepdim=True) / self.mask.sum()
         mask_std = torch.sqrt(torch.sum(((x - mask_mean) ** 2) * self.mask, (-1, -2), keepdim=True) / self.mask.sum())
-        fixed_im = (x - x.mean()) * (self.std / (mask_std + 1e-9)).view(len(x), *[1, ] * (x.dim() - 1)) + self.mean
-        return fixed_std
+        fixed_im = (x - mask_mean) * (self.std / (mask_std + 1e-9)).view(len(x), *[1, ] * (x.dim() - 1)) + self.mean
+        return fixed_im
